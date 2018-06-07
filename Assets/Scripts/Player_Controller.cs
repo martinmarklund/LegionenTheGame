b19251a7193b21c0daa24895de738e85051512e9
Martin Marklund <martin.marklund@hotmail.se>
2018-06-07 11:02:09 +0200

    Set up Assets folders. Started working on script Player_Controller.

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    // Public variables
+    public double gravity = 9.8;
+    public float verticalSpeed = 0;
+
+    // Private variables
+    private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+
+        controller = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(controller.isGrounded)
+        {
+            verticalSpeed = 0;
+        }
+
+
+	}
+}
