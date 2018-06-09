c6228d3c34ff497ccc994d42605be0e7e2c43cd2
Martin Marklund <martin.marklund@hotmail.se>
2018-06-09 16:08:38 +0200

    Implemented basic character movement. Jumping still needs to be worked on. Created Idle and Walking animations for Wolf using provided SpriteSheets.

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -5,39 +5,79 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour {
 
     // Public variables
-    public float speed = 5;
-    public float gravity = 10;
-    public float verticalSpeed = 0;
-    public float jumpSpeed = 8;
+    public float topSpeed = 2.0f;       // How fast the player can move
+    public float jumpForce = 700.0f;    // Force applied to player when jumping
+    public Transform groundCheck;       // Transform at player's feet to check if player is grounded
+    public LayerMask whatIsGround;     // What layer is considered the ground
 
     // Private variables
-    private CharacterController controller;
-    private Vector2 velocity;
+    private float move;                 // Float holding move direction
+    private bool facingRight = true;    // Is the player facing right
+    private Animator animator;          // Reference to animator
+    private bool grounded = false;      // Not grounded by default
+    public float groundRadius = 0.2f;  // The radius of the circle used for checking the distance to the ground
 
-	// Use this for initialization
-	void Start () {
 
-        controller = GetComponent<CharacterController>();
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        groundCheck = GetComponent<Transform>();
+        whatIsGround = 8;
+        Debug.Log(whatIsGround.value);
+    }
+    // Physics will be manipulated at the end of each fram in fixed update
+    void FixedUpdate()
+    {
+        Debug.Log(groundCheck.position);
+        // Will return true or false depending on whether groundCheck hit whatIsGround with the groundRadius
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        velocity = transform.forward * Input.GetAxis("Vertical") * speed;
+        //animator.SetBool("Ground", grounded);
+        //animator.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y); 
         
-        if(controller.isGrounded)
+        // Get move directions
+        move = Input.GetAxis("Horizontal");
+
+        // Adds velocity to the Rigidbody in the move direction multiplied with speed.
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        animator.SetFloat("Speed", Mathf.Abs(move));
+
+        // Check if the sprite needs to be flipped
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+
+        // Can the player jump, if yes: add jump force
+        if (grounded && Input.GetKeyDown(KeyCode.Z))
         {
-            verticalSpeed = 0;
-            if (Input.GetKeyDown("space"))
-            {
-                verticalSpeed = jumpSpeed;
-            }
+            animator.SetBool("Grounded", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
         }
 
-        verticalSpeed -= gravity * Time.deltaTime;
-        velocity.y = verticalSpeed;
-        controller.Move(velocity * Time.deltaTime);
+    }
 
-	}
+    void Update()
+    {
+
+    }
+
+
+    /// <summary>
+    /// Flips the sprite over the x axis
+    /// </summary>
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        // Get local scale and flip over the x axis
+        Vector3 theScale = transform.localScale;
+
+        theScale.x *= -1;
+
+        // Update local scale
+        transform.localScale = theScale;
+
+    }
 }
