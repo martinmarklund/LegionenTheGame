e9b43d6c9efa3ca980fd721800298327d7d8a0fa
Martin Marklund <martin.marklund@hotmail.se>
2018-07-27 14:01:41 +0200

    Perfected jumping animation, added damage animation, fixed Ladder script and made it compatible with Player_Script.

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+
+    public float speed = 3.0f;
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        
+
+        if (other.tag == "Player" && Input.GetKey(KeyCode.UpArrow))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+
+        }
+        else if (other.tag == "Player" && Input.GetKey(KeyCode.DownArrow))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+    }
+}
